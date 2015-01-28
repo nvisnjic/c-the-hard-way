0bd28f1d93c6c3b3522a6bf150626e8346318dae
Niko Visnjic <visnjic.niko@gmail.com>
2015-01-28 15:04:14 +0800

    the hard way

---
+++ ex1/notes.md
@@ -0,0 +1,10 @@
+Exercise 1
+
+
+make {name of .c file} is a shorthand for gcc {name} -o {name}
+
+make ex1
+
+Warnings are also cool. Warningall on.
+
+CFLAGS="-Wall" make ex1
---
+++ ex2/ex1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+
+    puts("Hello World");
+    printf("This is program %s, it doesnt need any args: but there are %d\n", argv[0], argc);
+    return 0;
+}
+
