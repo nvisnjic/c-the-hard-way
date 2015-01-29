a2e9d3c2c3cd75477d2258395e9366f9599f33c7
Niko Visnjic <visnjic.niko@gmail.com>
2015-01-29 17:25:57 +0800

    ex5 done

---
+++ ex5/README.md
@@ -0,0 +1,7 @@
+Exercise 5
+==========
+<http://c.learncodethehardway.org/book/ex5.html>
+
+Basic stuff...
+Valgrind more.
+
---
+++ ex5/ex5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/* Multi line commenting time */
+int main(int argc, char *argv[]){
+
+    int distance = 668;
+
+    //some other commenting and basic stuff
+    printf(" Distance from the beast: %d \n", distance);
+
+    return 0;
+}
