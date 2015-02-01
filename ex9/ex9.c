0c16398218695bab71b7aa6cd97262994bd60fab
Niko Visnjic <visnjic.niko@gmail.com>
2015-02-01 22:43:06 +0800

    ex9 edit

--- ex9/ex9.c
+++ ex9/ex9.c
@@ -1,4 +1,16 @@
-#include <stdio.h>
+#include<stdio.h>
+
+// a little bit of masking and byte printing
+#define BYTETOBINARYPATTERN "%d%d%d%d%d%d%d%d"
+#define BYTETOBINARY(byte)  \
+  (byte & 0x80 ? 1 : 0), \
+  (byte & 0x40 ? 1 : 0), \
+  (byte & 0x20 ? 1 : 0), \
+  (byte & 0x10 ? 1 : 0), \
+  (byte & 0x08 ? 1 : 0), \
+  (byte & 0x04 ? 1 : 0), \
+  (byte & 0x02 ? 1 : 0), \
+  (byte & 0x01 ? 1 : 0) 
 
 int main(int argc, char *argv[])
 {
@@ -28,11 +40,32 @@ int main(int argc, char *argv[])
     name[2] = 'd';
     name[3] = '\0';
     
-    
+    // alternate name as integer (4 bytes = 4 chars)
+
+    //name[0] = 
+
     // then print them out initialized
     printf("numbers: %d %d %d %d\n",
             numbers[0], numbers[1],
             numbers[2], numbers[3]);
+
+    
+    // then print them out initialized
+    printf("name in nums: %d %d %d %d\n",
+            name[0], name[1],
+            name[2], name[3]);
+
+    int i = 0;
+    // then print them out initialized
+    printf("name in bits\n");
+    for(; i < 4; i++){
+        printf("name[%d]: "BYTETOBINARYPATTERN"\n", 
+            i, BYTETOBINARY(name[i]));
+    }
+   
+    // print the string Zed/0 as a integer
+    // address pointer pointing to name[0] cast as an integer pointer
+    printf("dat integer = %d \n", *((int *) &name[0]));
 
     printf("name each: %c %c %c %c\n",
             name[0], name[1],
